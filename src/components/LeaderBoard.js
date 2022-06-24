85a1f4788bbe864b4bd6683e835798354242321e
Harsha Pamidipalli <sriharsha1905@gmail.com>
2022-06-24 16:41:50 +0530

    https:// -> //domain/resource

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -18,7 +18,8 @@ const LeaderBoard =()=>{
 	useEffect(() => {
 		const getSessionDetails =async () => {
 			console.log('requesting from', `${process.env.REACT_APP_API}/leaderboard`);
-			let leaderURL = `${process.env.REACT_APP_API}/leaderboard`;
+			//let leaderURL = `${process.env.REACT_APP_API}/leaderboard`;
+			let leaderURL = '//mcdbackend.indicwikidsp.tk/leaderboard';
 			if(userAuthToken){
 				leaderURL = `${leaderURL}?auth_token=${userAuthToken}/`;
 			}
